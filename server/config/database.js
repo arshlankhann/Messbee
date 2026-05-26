105180c5f0254f5481d25dbde1f579fdd58d8968
Arshlan Khan <arshlank894@gmail.com>
2026-05-26 12:54:58 +0530

    fixed terminal warning

--- server/config/database.js
+++ server/config/database.js
@@ -9,10 +9,7 @@ const connectDB = async () => {
     const uri = process.env.MONGODB_URI;
     if (!uri) throw new Error('MONGODB_URI is not defined');
 
-    const conn = await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(uri);
 
      console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
